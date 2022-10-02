5e725bdac0d4ea1b7834f7312130426f370c128a
Matheus Francisco <matheusfrancisco1103@gmail.com>
2022-10-02 20:27:45 -0300

    handle some errors in divide function

--- web-app/main.go
+++ web-app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -23,10 +24,35 @@ func addValues(x, y int) int {
 	return x + y
 }
 
+// Divide is a page
+func Divide(w http.ResponseWriter, r *http.Request) {
+	var x, y float32
+
+	x = 100.0
+	y = 0.0
+	f, err := division(x, y)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is equal to %f", x, y, f))
+}
+
+// division divides two float32 and return de result and the error if exists
+func division(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 // main is the main application function
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("starting application on port %s", portNumber))
 	_ = http.ListenAndServe(portNumber, nil)
