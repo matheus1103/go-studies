a5ca81117520a5d8766cfa09e1a221a187122748
Matheus Francisco <matheusfrancisco1103@gmail.com>
2022-11-09 16:02:27 -0300

    build template cache

---
+++ web-app/cmd/web/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/matheus1103/go-studies/pkg/handlers"
+	"net/http"
+)
+
+const portNumber = ":8080"
+
+// main is the main application function
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Printf("starting application on port %s", portNumber)
+	_ = http.ListenAndServe(portNumber, nil)
+}
