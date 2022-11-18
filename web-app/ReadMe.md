7c46190aca509ae7c4a9af6830c9c702d26e1157
Matheus Francisco <matheusfrancisco1103@gmail.com>
2022-11-17 23:58:55 -0300

    Adding template data and Readme

---
+++ web-app/ReadMe.md
@@ -0,0 +1,3 @@
+# Golang Studies
+
+## Web Application
--- web-app/cmd/web/main.go
+++ web-app/cmd/web/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/matheus1103/go-studies/pkg/config"
 	"github.com/matheus1103/go-studies/pkg/handlers"
+	"github.com/matheus1103/go-studies/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -10,8 +13,23 @@ const portNumber = ":8080"
 
 // main is the main application function
 func main() {
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	var app config.AppConfig
+
+	tc, err := render.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache")
+	}
+
+	app.TemplateCache = tc
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	render.NewTemplates(&app)
+
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	fmt.Printf("starting application on port %s", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
