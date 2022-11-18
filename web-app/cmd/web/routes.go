11f468b8d8c4b1c91c8e4e5e09800f65fa76083c
Matheus Francisco <matheusfrancisco1103@gmail.com>
2022-11-18 00:31:25 -0300

    ### lesson 48 - Adding middlewares Nosurf and routing with chi

---
+++ web-app/cmd/web/routes.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/matheus1103/go-studies/pkg/config"
+	"github.com/matheus1103/go-studies/pkg/handlers"
+	"net/http"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
+
+	return mux
+}
